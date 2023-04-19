59210400595730263913869a5e9f25289ba51b77
Jorge <jorgechavez.0403@gmail.com>
2023-04-19 09:09:01 -0600

    JORGE_001
    bucles

--- 2023_04_17_006_bucles_Jorge_Delgadillo/2023_04_17_006_bucles_Jorge_Delgadillo.cpp
+++ 2023_04_17_006_bucles_Jorge_Delgadillo/2023_04_17_006_bucles_Jorge_Delgadillo.cpp
@@ -15,7 +15,9 @@ int main()
     int opc = 0;
     int rr = 0;
     int ED = 0;
-    int GSF = 0;
+    int G = 0;
+    int S = 0;
+    int F = 0;
     std::cout << "Holla amigo hoy tendras la oportunidad de peliar por tu vida\n" << std::endl;
     std::cout << "Contra cuantos seres capases como tu quieres peliar hoy?\n" << std::endl;
     std::cin >> competidores;
@@ -31,12 +33,25 @@ int main()
             std::cout << "tendremos " << rr << " batallas en total te deseo suerte la ocuparas" <<std::endl;
             break;
         case 2:
+            
+            ED = (competidores - 1);
 
-            std::cout << "battle royal\n";
-            std::cout << "";
+            std::cout << "battle royal\n" << std::endl;
+            std::cout << "Que bueno me pregunto cuantas peleas seran en total " << ED << " a veo te deceo suerte la ocuparas " << std::endl;
+
             break;
         case 3:
-            std::cout << "batalla por equipos\n";
+
+            G = (competidores / 4);
+            for (int t = 1; t < G; t++) {
+                rr = (G - t) + rr ;
+
+            }
+            F = rr * 4;
+            S = F + 3;
+
+            std::cout << "batalla por equipos\n" << std::endl;
+            std::cout << "battallas por equipo e cuantas battallas " << S << " en ese caso que el mejor equipo gane" << std::endl;
             break;
 
         }
