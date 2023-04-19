d111dd0f16a5a358156128d5464c33602dcac03f
Jorge <jorgechavez.0403@gmail.com>
2023-04-19 10:52:59 -0600

    JOrge_001
    valor absoluto

---
+++ 2023_04_19_Vslor_absoluto_v1/2023_04_19_Vslor_absoluto_v1.cpp
@@ -0,0 +1,19 @@
+// 2023_04_19_Vslor_absoluto_v1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//De Jorge Alejandro Delgadillo
+
+#include <iostream>
+
+int main()
+{
+	int dato1 = 0;
+    std::cout << "Hola este prog5rama es para sacar el valor absoluto de un numero entero\n" << std::endl;
+	std::cout << "Inserte su numero" << std::endl;
+	std::cin >> dato1;
+	if (dato1 <= 0) {
+		dato1 < -dato1 * (-1)
+	}
+		if (dato1 >= 0 )
+		dato1 < -dato1
+		FinSi
+		Escribir 'el valor absoluto es:', resultado, ' '
+}
