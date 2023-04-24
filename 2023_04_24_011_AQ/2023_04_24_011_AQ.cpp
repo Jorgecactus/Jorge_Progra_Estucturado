ee31f9a5a95af6fe78ac41039571c35522642e40
Jorge <jorgechavez.0403@gmail.com>
2023-04-24 10:03:07 -0600

    Jorge_011
    adivino tu personaje

---
+++ 2023_04_24_011_AQ/2023_04_24_011_AQ.cpp
@@ -0,0 +1,12 @@
+// 2023_04_24_011_AQ.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//DE JORGE ALEJANDRO DELGADILLO CHÁVEZ
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "HOLA HOY INTENTARE ADIVINAR TU PERSONAJE FAVORITO DE \n" << std::endl;
+
+
+}
