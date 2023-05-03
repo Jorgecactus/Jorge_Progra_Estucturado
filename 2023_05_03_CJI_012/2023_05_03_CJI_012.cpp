374f367b55e9399a0b51deb80cd7b0a4aafbc2cf
Jorge <jorgechavez.0403@gmail.com>
2023-05-03 09:01:17 -0600

    Jorge_001
    vucle infinito

---
+++ 2023_05_03_CJI_012/2023_05_03_CJI_012.cpp
@@ -0,0 +1,32 @@
+// 2023_05_03_CJI_012.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    int opc;
+    int vidas = 3;
+    bool continuar = true;
+    char respuesta;
+    std::cout << "opciones 1- vucle de juego infinito 2- contador sel 0 al 100\n"<< std::endl;
+    std::cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        std::cout << "Bien pelearemos asta que pierdas tus tres vidas \n"<< std::endl;
+        
+        
+
+        break;
+        system("cls");
+    case 2:
+
+        for (int i = 0; i<=100; i++)  
+        {
+            std::cout << "El contador va en: " << i << std::endl;
+        }
+
+        break;
+    }
+}
