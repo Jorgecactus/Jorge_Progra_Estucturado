32f302cf963b1c9a66abbef15b3690f48f339b52
Jorge <jorgechavez.0403@gmail.com>
2023-05-04 10:33:38 -0600

    Jorge_014
    del 1000 al 0

--- 2023_04_27_014_NOYCH/2023_04_27_014_NOYCH.cpp
+++ 2023_04_27_014_NOYCH/2023_04_27_014_NOYCH.cpp
@@ -8,64 +8,30 @@
 
 int main()
 {
-    int aux = 3, aux2 = 0;
+  
     int opc = 0;
-    int num1 = 0, num2 = 0, res = 0;
 
 
     while (true)
     {
-        std::cout << "Hola que quieres hacer? 1.- Calculadora con creditos \n2.- Contar hasta 100 lento \n3.- Contar de 10 en 10\n ";
+        std::cout << "Hola que quieres hacer? 1.- contar de 0 a 1000 \n2.- Contar hasta 100 lento \n3.- Contar de 10 en 10\n ";
         std::cin >> opc;
         switch (opc)
         {
-        case 1: // Calculadora
-            
-            while (aux != 0)
+        case 1: // contandor de 1000 al 0
+            std::cout << "Estas en el contador del 1000 al 0\n";
+            std::cout << "con cual ciclo quiere hacer esto 1- for 2- while 3- do while\n";
+            std::cin >> opc;
+            switch (opc)
             {
-                //Algoritmo de la calculadora
-                std::cout << "Ingresa 1 para suma 2 para resta\n";
-                std::cin >> opc;
-                switch (opc)
+            case 1:
+                for (int i = 1000; i >= 0; i--)
                 {
-                case 1:
-                    std::cout << "Dame el primer numero a sumar\n";
-                    std::cin >> num1;
-                    std::cout << "Dame el segundo numero a sumar\n";
-                    std::cin >> num2;
-                    res = num1 + num2;
-                    std::cout << "El resultado de la suma es: " << res << std::endl;
-                    break;
-                case 2:
-                    std::cout << "Dame el primer numero a restar\n";
-                    std::cin >> num1;
-                    std::cout << "Dame el segundo numero a restar\n";
-                    std::cin >> num2;
-                    res = num1 - num2;
-                    std::cout << "El resultado de la resta es: " << res << std::endl;
-                    break;
-                }
-                std::cout << "Gastaste un crédito\n";
-                aux--;
-            }
-            std::cout << "Terminaste tus créditos\n";
-            //algoritmo para recargar creditos o continuar.
-            std::cout << "Quieres recargar tus créditos?\n 1.- Si 2.- No";
-            std::cin >> aux2;
-            if (aux2 == 1)
-            {
-                aux = 3;
-                std::cout << "Tus créditos son: " << aux << std::endl;
-            }
-            else if (aux2 == 2)
-            {
-                std::cout << "Que le vaya bien\n";
-            }
-            else
-            {
-                std::cout << "no recibi una respuesta valida bay\n";
-            }
+                    std::cout << "El contador va en: " << i << std::endl;
 
+                }
+                break;
+            }
             break;
         case 2: // Contar hasta 100
             std::cout << "Estas en el contador hasta 100\n";
